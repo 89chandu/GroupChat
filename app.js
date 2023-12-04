1de0879fdc272b97fa638d40def181bc92a8e285
“Chandu” <Chandubopche321@gmail.com>
2023-12-04 12:48:30 +0530

    Creating the ChatApp Backend

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const bodyParser = require('body-parser');
 const sequelize=require('./util/database');
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin:"*"
+    }
+));
 
 const userRoute=require('./routes/user');
+const messageRoute=require('./routes/message');
  
 app.use(bodyParser.json());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/user',userRoute);
+app.use('/message',messageRoute);
 
 sequelize
         .sync()
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const userController = require('../controllers/user');
+const userauthentication=require('../middlewares/auth');
 
 const router = express.Router();
 
+router.get('/:UserId',userController.getuser);
+
 router.post('/signup',userController.signup);
+
+router.post('/login',userController.login);
 
 module.exports = router; 
