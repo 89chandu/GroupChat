f5141301f4f8c40c25c0dfdabd7eb1df064f2ce1
Chandu Bopche <82381986+89chandu@users.noreply.github.com>
2023-12-06 17:15:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-
 require('dotenv').config()
-
 const express=require('express')
 const cors=require('cors')
 const bodyParser=require('body-parser')
 const path=require('path')
 const fs=require('fs')
 
-
-
 const db=require('./database')
 const userRoutes=require('./Routes/user')
 const chatRoutes=require('./Routes/chat')
 const groupRoutes=require('./Routes/group')
-
 
 const User=require('./Models/User')
 const Chat=require('./Models/Chat')
@@ -24,24 +19,13 @@ const Group=require('./Models/Group')
 
 const socketServer=require('./Server-Socket/server')
 
-
-
-
-
-
-
 app=express()
-
-
-
 
 app.use(cors({
     origin:'*',
     methods:['GET','POST']
 }))
 app.use(bodyParser.json({extends:false}))
-
-
 
 app.use('/User',userRoutes)
 app.use('/Chat',chatRoutes)
@@ -67,6 +51,6 @@ GroupMessage.belongsTo(Group)
 db.sync({alter:true})
 .then(()=>{
     
-app.listen(3100)
+app.listen(80)
 })
 .catch(err=>console.log(err))
